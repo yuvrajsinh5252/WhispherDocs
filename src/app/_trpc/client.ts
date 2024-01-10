2f4f6733b4271f02ead615079e87c2be1e300eab
yuvrajsinh5252 <yuvrajsinh476@gmail.com>
2024-01-10 22:36:21 +0530

    restarted

--- src/app/_trpc/client.ts
+++
@@ -1,4 +0,0 @@
-import { AppRouter } from "@/trpc";
-import { createReactQueryHooks } from "@trpc/react-query";
-
-export const trpc = createReactQueryHooks<AppRouter>();
--- src/app/auth-callback/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function page() {
-    return (
-        <div>
-            <h1>Auth Callback</h1>
-        </div>
-    );
-}
