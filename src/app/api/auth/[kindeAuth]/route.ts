ec3f42590a13da47b1fe15dbba6205bf45c314bb
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-01-30 12:12:25 +0530

    deploy

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, { params }: any) {
+export async function GET(request: NextRequest, { params }: { params: { kindeAuth: string } }) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 }
