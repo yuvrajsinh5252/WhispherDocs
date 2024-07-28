e7ad255417447fc6ff898022d04330a3199beaa0
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-07-29 00:04:54 +0530

    generating response not using pdf

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -36,43 +36,48 @@ export const POST = async (req: NextRequest) => {
   });
 
   // vectorize message
-  const embeddings = new CohereEmbeddings({
-    apiKey: process.env.COHERE_API_KEY,
-    model: "embed-english-v3.0",
-  });
+  // const embeddings = new CohereEmbeddings({
+  //   apiKey: process.env.COHERE_API_KEY,
+  //   model: "embed-english-v3.0",
+  // });
 
-  const pinecone = await PineconeClient();
-  const pineconeIndex = pinecone.Index("cohere-pinecone-trec");
+  // const pinecone = await PineconeClient();
+  // const pineconeIndex = pinecone.Index("cohere-pinecone-trec");
 
-  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-    pineconeIndex,
-    namespace: file.id,
-  });
+  // const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  //   pineconeIndex,
+  //   namespace: file.id,
+  // });
 
-  const results = await vectorStore.similaritySearch(message, 4);
+  // const results = await vectorStore.similaritySearch(message, 4);
 
-  const prevMessages = await db.messages.findMany({
-    where: {
-      fileId: file.id,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-    take: 6,
-  });
+  // const prevMessages = await db.messages.findMany({
+  //   where: {
+  //     fileId: file.id,
+  //   },
+  //   orderBy: {
+  //     createdAt: "asc",
+  //   },
+  //   take: 6,
+  // });
 
   const cohere = new CohereClient({
     token: process.env.COHERE_API_KEY,
   });
 
-  const response = await cohere.chatStream({
-    chatHistory: prevMessages.map((m) => ({
-      role: m.isUserMessage ? "USER" : "CHATBOT",
-      message: m.text,
-    })),
-    message: message,
-    // conversationId: file.id,
+  const response = await cohere.generate({
+    prompt: message,
+  });
+
+  await db.messages.create({
+    data: {
+      text: response.generations[0].text,
+      isUserMessage: false,
+      userId: user?.id,
+      fileId: fileId,
+    },
   });
 
   // returned the streamed response and let the client handle the streaming
+  return response.generations[0].text;
 };
